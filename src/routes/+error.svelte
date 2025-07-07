fbfbda533d75697f5135a885159212e1a778eb1a
Jaiden <jaidenengland@gmail.com>
2025-07-07 21:07:19 +0100

    custom error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
     import { page } from '$app/stores';
+
 </script>
 
-<h1>{$page.status}: {$page.error?.message}</h1>
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        flex-direction: column;
+        gap: 50px;
+    }
+</style>
+
+<body>
+    <div class="container">
+        <img src="https://http.cat/images/{$page.status}.jpg" alt="" id="ErrorImage" />
+        <a href="/">Home</a>
+    </div>
+</body>
